98704843eea8d1218a05799f2b55c8bc4338d8fa
max <max.panoff@gmail.com>
2018-09-04 11:46:03 -0400

    end class

--- inclass/class1/class1.cpp
+++ inclass/class1/class1.cpp
@@ -4,7 +4,31 @@
 
 int main ()
 {
-  std::cout << "Hello World! \n";
+//declare varibles
+  int x = 10;
+  float y = 3.1456;
+  char c = 'y';
 
+// I/O
+  std::cout << "Hello World! \n";
+  std::cin >> x;
+
+//equations
+  x = x + 1;
+
+//loops
+  y = 1/y;
+  for(int i =0; i<5; i++)
+  {
+   float x = x*y;
+  }
+  y = 1/y;
+
+  while (x <= 0)
+  {
+    x++;
+  }
+// return values and end program
+  std::cout << x << "\n" << y << "\n";
   return 1;
 }
