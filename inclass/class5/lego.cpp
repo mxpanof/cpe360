48ca125c340587b887e747acb916bf34ccddbf27
max <max.panoff@gmail.com>
2018-09-20 10:58:13 -0400

    class5

---
+++ inclass/class5/lego.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class legoChunk
+{
+	public:
+	int x, y;
+	legoChunk * next, *prev;
+	legoChunk (int xin = 0, int yin = 0):x(xin),y(yin)
+	{
+		next=prev=NULL;
+	}
+};
+
+int main ()
+{
+	//vector<legoChunk> legoList;
+	legoChunk * ptr;
+	ptr = new legoChunk;
+	ptr->x = 50;
+	ptr->y = 90;
+	//legoList.push_back(*ptr);
+	legoChunk * prevPtr;
+	
+	prevPtr = ptr;
+	ptr->next=new legoChunk;
+	ptr = ptr->next;
+	ptr->prev = prevPtr;
+	cout << &ptr->prev + sizeof (legoChunk) << "\n" << &ptr;
+	
+	return 0;
+}
